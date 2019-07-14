026a108e39f50ccef158d3651136623140ef9003
mfrashad <m.fathyrashad@gmail.com>
2019-07-14 21:59:55 +0800

    change readme, and enable auth

--- src/core/navigation/routes.tsx
+++ src/core/navigation/routes.tsx
@@ -37,7 +37,7 @@ const MenuNavigator: NavigationContainer = createBottomTabNavigator({
 });
 
 const AppNavigator: NavigationContainer = createStackNavigator({
-  //...AuthNavigationMap,
+  ...AuthNavigationMap,
   ['Home']:MenuNavigator,
 }, {
   headerMode: 'screen',
