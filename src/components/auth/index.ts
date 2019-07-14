9bcb61d48542e44bda835dde064859f73aacf9dc
mfrashad <m.fathyrashad@gmail.com>
2019-07-14 16:04:28 +0800

    Cleanup 3

--- src/components/auth/index.ts
+++ src/components/auth/index.ts
@@ -4,30 +4,10 @@ export {
   ForgotPasswordFormData,
 } from './forgotPassword';
 export {
-  SignInForm1,
-  SignInForm1Props,
-  SignInForm1Data,
-} from './signInForm1';
-export {
   SignInForm2,
   SignInForm2Props,
   SignInForm2Data,
 } from './signInForm2';
-export {
-  SignInForm3,
-  SignInForm3Props,
-  SignInForm3Data,
-} from './signInForm3';
-export {
-  SignInForm4,
-  SignInForm4Props,
-  SignInForm4Data,
-} from './signInForm4';
-export {
-  SignUpForm1,
-  SignUpForm1Props,
-  SignUpForm1Data,
-} from './signUpForm1';
 export {
   SignUpForm2,
   SignUpForm2Props,
--- src/components/social/index.ts
+++ src/components/social/index.ts
@@ -1,24 +1,4 @@
 export {
-  ProfileLocation,
-  ProfileLocationProps,
-} from './profile/profileLocation.component';
-export {
-  ProfileParameter,
-  ProfileParameterProps,
-} from './profile/profileParameter.component';
-export {
   ProfileParameterCard,
   ProfileParameterCardProps,
 } from './profile/profileParameterCard.component';
-export {
-  ProfileSocials,
-  ProfileSocialsProps,
-} from './profile/profileSocials.component';
-export {
-  FeedActivityBar,
-  FeedActivityBarProps,
-} from './feed/feedActivityBar.component';
-export {
-  Styx,
-  StyxProps,
-} from './feed/styx.component';
